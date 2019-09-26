68819077a31380fd104c0b2d8f94a3819f3d8466
rashmimilan <48384401+rashmimilan@users.noreply.github.com>
2019-09-26 14:53:18 +0530

    Update DynamicTableSkeleton.js

--- source_code/src/DynamicTableSkeleton.js
+++ source_code/src/DynamicTableSkeleton.js
@@ -3,7 +3,6 @@ class DynamicTableSkeleton extends Component {
    constructor(props){
    super(props);    
    this.getColumnTitles = this.getColumnTitles.bind(this);
-   this.getValidColumnTitles = this.getValidColumnTitles.bind(this);   
    this.getRows = this.getRows.bind(this);
    this.extractKeys = this.extractKeys.bind(this);
 }
@@ -13,19 +12,15 @@ extractKeys= function(){
 }
 
 getColumnTitles = function(){
-   var key_params = Object.keys(this.props.table_params[0]);
-   return key_params.map((each_column, value)=>{
-   if(this.props.table_params[0][each_column] === "yes")
-   return <th>{each_column}</th>
- })
-}
-
-getValidColumnTitles = function(each_column){
-   var key_params = Object.keys(this.props.table_params); 
-   return key_params.map((each_param_column, value)=>{ 
-   return <td>{each_column} - {this.props.table_params[0][each_column]}</td>
-  })
-    //return (each_column.toUpperCase())
+   var columns = this.extractKeys();
+   return columns.map((each_column, value)=>{
+   if(this.props.table_params !== undefined) {  
+   if(this.props.table_params[0][each_column])
+   return <th>{each_column.toUpperCase()}</th>
+   } else {
+    return <th>{each_column.toUpperCase()}</th>
+   }
+   })
 }
 
 getRows = function(){
@@ -43,8 +38,12 @@ getEachRow = function(each_row){
    const class_red = { color:'red'};
    const class_name = each_row.show ? class_blue : class_red;
    return table_headers.map((key, value)=>{
-    if(this.props.table_params[0][key]) 
+   if(this.props.table_params !== undefined) {  
+   if(this.props.table_params[0][key]) 
    return <td style={class_name}>{each_row[key]}</td>
+   } else {
+   return <td style={class_name}>{each_row[key]}</td>  
+   }
   })
 }
 
